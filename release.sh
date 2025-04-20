fc5f053c5844cc614c61f53705cd26189df9de17
Sam Anthony <sam@samanthony.xyz>
2025-04-19 20:09:13 -0400

    release script

---
+++ release.sh
@@ -0,0 +1,21 @@
+version=$(git describe --tags)
+
+gooses=(darwin linux windows)
+goarches=(amd64 arm64)
+
+if [ ! -d bin ]; then
+	mkdir bin
+fi
+
+for goos in "${gooses[@]}"; do
+	for goarch in "${goarches[@]}"; do
+		file="hose_${version}_${goos}_${goarch}"
+		if [ "$goos" = "windows" ]; then
+			file="$file.exe"
+		fi
+		echo $file
+		GOOS=$goos GOARCH=$goarch go build -o "bin/$file"
+	done
+done
+
+echo done
